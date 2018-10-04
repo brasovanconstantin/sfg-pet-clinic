9e7f2a64907dfb4ace63c2a5e225886a003fffb6
Constantin Brasovan <constantin.brasovan@endava.com>
2018-10-04 11:14:35 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/train/sprinframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package train.sprinframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/train/sprinframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package train.sprinframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
