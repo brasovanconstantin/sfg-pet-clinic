7004bf66d18b765469980cc0b47dfc14efe16ac6
Constantin Brasovan <constantin.brasovan@endava.com>
2018-10-29 09:20:25 +0200

    Added Specialities Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends BaseCrudService<Speciality, Long> {
+
+
+}
